b121e5f4c7ac83ed7911d63d1ebed12774acbb7c
IsaacYongTY <isaac.yongty@gmail.com>
2021-03-17 02:40:49 +0800

    add toggle decimal function and minor style

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useRef } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
+
 export default function App() {
 
     const [ bpm, setBpm ] = useState(75)
+    const [ displayBpm, setDisplayBpm ] = useState(0)
     const [ count, setCount ] = useState(0)
     const [ bpmColor, setBpmColor ] = useState('rgb(0,0,0)')
-
+    const [ isPlaying, setIsPlaying ] = useState(false)
+    const [ isDecimal, setIsDecimal ] = useState(true)
     let startTime = useRef(0)
     let totalTimeLapsed = useRef(0)
+
 
     const calculateBPM = () => {
 
@@ -30,6 +34,7 @@ export default function App() {
 
             if (count > 1) {
                 setBpm((1/(averageTimeLapsed/60000)).toFixed(1))
+                setDisplayBpm((1/(averageTimeLapsed/60000)).toFixed(1))
                 setBpmColor('rgb(0,0,0)')
             }
         }
@@ -41,35 +46,67 @@ export default function App() {
 
     const handleReset = () => {
         setBpm(0)
+        setDisplayBpm(0)
         setCount(0)
         totalTimeLapsed.current = 0
         startTime.current = Date.now()
     }
 
-      return (
+    const handlePlay = () => {
+        console.log('Button is tapped')
+
+
+        if(!isPlaying) {
+
+            setIsPlaying(true)
+        } else {
+
+            setIsPlaying(false)
+        }
+
+    }
+
+    const toggleDecimal = () => {
+            setIsDecimal(!isDecimal)
+            setDisplayBpm(Number(bpm).toFixed(!isDecimal ? 1 : 0))
+
+
+    }
+    return (
         <View style={styles.container}>
-          <Text style={[styles.metronomeNumStyle, { color: `${bpmColor}`}]}>{bpm}</Text>
-          <Button title="Tap" onPress={calculateBPM}/>
-          <Button title="Reset" onPress={handleReset} />
-          <Button title="Show Decimal" />
-          <Button title="Play" />
-          <Text>{count}</Text>
-          <StatusBar style="auto" />
+            <View style={styles.metronomeContainerStyle}>
+                <Text style={[styles.metronomeNumStyle, { color: `${bpmColor}`}]}>{isDecimal ? bpm : displayBpm}</Text>
+            </View>
+
+            <Button title="Tap" onPress={calculateBPM}/>
+            <Button title="Reset" onPress={handleReset} />
+            <Button title="Show Decimal" onPress={toggleDecimal} />
+            <Button title="Play" onPress={handlePlay}/>
+            <Text>{count}</Text>
+            <StatusBar style="auto" />
         </View>
-      );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f4f19a',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+        container: {
+        flex: 1,
+        backgroundColor: '#f4f19a',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 
-  metronomeNumStyle: {
-    fontSize: 72,
+    metronomeContainerStyle: {
 
-
-  }
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        backgroundColor: 'lightblue',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    metronomeNumStyle: {
+        fontSize: 72,
+    }
 });
